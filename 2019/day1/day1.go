89561faafcabc1ddc12ca5d59f9107e06436e880
revikk <dima@DESKTOP-364GV83.localdomain>
2020-08-08 18:30:20 +0300

    First star for day one

--- 2019/day1/day1.go
+++ 2019/day1/day1.go
@@ -1,1 +1,33 @@
-package day1
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input")
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	var result float64 = 0
+	for scanner.Scan() {
+		mass, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		fuel := math.Trunc(float64(mass)/float64(3)) - 2
+		result += fuel
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result)
+}
