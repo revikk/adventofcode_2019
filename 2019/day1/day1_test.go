f78d2ca39e43e9d9225cfd6343e1f2f7cdf43bef
Dima <ponomarev_dm@mail.ru>
2020-08-13 20:07:22 +0300

    Add basic test for massToFuel

--- 2019/day1/day1_test.go
+++ 2019/day1/day1_test.go
@@ -4,6 +4,27 @@ import (
 	"testing"
 )
 
-func TestMassToFuel(t *testing.T) {
+type inputExpected struct {
+	input    float64
+	expected float64
+}
 
+func TestMassToFuel(t *testing.T) {
+	dataItems := []inputExpected{
+		{12, 2},
+		{14, 2},
+		{1969, 654},
+		{100756, 33583},
+	}
+
+	for _, item := range dataItems {
+		result := massToFuel(item.input)
+		checkResult(t, result, item.expected)
+	}
+}
+
+func checkResult(t *testing.T, result float64, expect float64) {
+	if result != expect {
+		t.Errorf("FAIL, expected %v, got %v", expect, result)
+	}
 }
