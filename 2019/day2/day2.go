023d73d646f9241cc668f18f27d93667f06bf895
Dima <ponomarev_dm@mail.ru>
2020-08-17 19:53:42 +0300

    Day2 Startup

--- 2019/day2/day2.go
+++ 2019/day2/day2.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err = scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
