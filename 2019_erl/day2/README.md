73bceeadd0e93905020c3e2cfa738c56ee57e1b0
revikk <ponomarev_dm@mail.ru>
2021-08-02 17:50:57 +0300

    Day2. Init

---
+++ 2019_erl/day2/README.md
@@ -0,0 +1,9 @@
+day2
+=====
+
+An OTP library
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ 2019_erl/day2/src/day2.erl
@@ -0,0 +1,31 @@
+-module(day2).
+
+-export([intcode/1]).
+
+intcode(Intcode) ->
+    do_intcode(1, Intcode).
+
+do_intcode(Position, Intcode) ->
+    Opcode = lists:nth(Position, Intcode),
+    do_opcode(Opcode, Position, Intcode).
+
+do_opcode(99, _, Intcode) ->
+    Intcode;
+do_opcode(1, Position, Intcode) ->
+    Value1 = lists:nth(Position + 1, Intcode),
+    Value2 = lists:nth(Position + 2, Intcode),
+    Sum = Value1 + Value2,
+
+    SumPosition = lists:nth(Position + 3, Intcode),
+    {Left, [_ForSumValue | Right]} = lists:split(SumPosition, Intcode),
+    NewIntcode = lists:append([Left, [Sum], Right]),
+    do_intcode(Position + 4, NewIntcode);
+do_opcode(2, Position, Intcode) ->
+    Value1 = lists:nth(Position + 1, Intcode),
+    Value2 = lists:nth(Position + 2, Intcode),
+    Multi = Value1 * Value2,
+
+    MultiPosition = lists:nth(Position + 3, Intcode),
+    {Left, [_ForMultiValue | Right]} = lists:split(MultiPosition, Intcode),
+    NewIntcode = lists:append([Left, [Multi], Right]),
+    do_intcode(Position + 4, NewIntcode).
