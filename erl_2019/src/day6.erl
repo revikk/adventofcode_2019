3271385c58eca521b6209efdeda84b4665da0785
revikk <ponomarev_dm@mail.ru>
2021-08-09 14:01:56 +0300

    Day6. Part 1 pass

--- erl_2019/src/day6.erl
+++ erl_2019/src/day6.erl
@@ -1,1 +1,29 @@
 -module(day6).
+
+-export([run/1]).
+
+run(OrbitMap) ->
+    {ok, BinData} = file:read_file(OrbitMap),
+    Orbits = binary:split(BinData, <<"\n">>, [global]),
+
+    Map = digraph:new([acyclic]),
+    lists:foreach(fun(Orbit) ->
+                     [Center, Satellite] = binary:split(Orbit, <<")">>),
+                     digraph:add_vertex(Map, Center),
+                     digraph:add_vertex(Map, Satellite),
+                     digraph:add_edge(Map, Satellite, Center)
+                  end,
+                  Orbits),
+
+    Planets = digraph:vertices(Map),
+
+    CenterOfMass = <<"COM">>,
+
+    lists:foldl(fun(Planet, NumOrbits) ->
+                   case digraph:get_short_path(Map, Planet, CenterOfMass) of
+                       false -> NumOrbits;
+                       Path -> length(Path) - 1 + NumOrbits
+                   end
+                end,
+                0,
+                Planets).
--- erl_2019/test/day6_tests.erl
+++ erl_2019/test/day6_tests.erl
@@ -1,3 +1,6 @@
--module(day6_test).
+-module(day6_tests).
 
 -include_lib("eunit/include/eunit.hrl").
+
+run_test_() ->
+    [?_assertEqual(42, day6:run("priv/day6_test_input"))].
