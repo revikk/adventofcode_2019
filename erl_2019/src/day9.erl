55ec403493bbd1ded7a39457c7c8d70912ec9821
revikk <ponomarev_dm@mail.ru>
2021-08-20 19:32:34 +0300

    Day9, part2 pass

--- erl_2019/src/day9.erl
+++ erl_2019/src/day9.erl
@@ -1,10 +1,14 @@
 -module(day9).
 
--export([run/1, get_first_star/0]).
+-export([run/1, get_first_star/0, get_second_star/0]).
 
 get_first_star() ->
     {ok, [Intcode]} = file:consult("priv/day9_input"),
     day5:intcode(1, Intcode).
+
+get_second_star() ->
+    {ok, [Intcode]} = file:consult("priv/day9_input"),
+    day5:intcode(2, Intcode).
 
 run(Intcode) ->
     day5:intcode(Intcode).
